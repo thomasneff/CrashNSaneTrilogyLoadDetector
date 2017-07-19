1ef1c6a840f3f6ea1a34127270edb40b5d1fb971
thomasneff <thomasneff93@gmail.com>
2017-07-19 23:29:27 +0200

    modified / refactored the whole program, added multiple feature vectors to increase robustness

---
+++ ASLScript/CrashNSaneLoadRemoval.asl
@@ -0,0 +1,61 @@
+﻿state("LiveSplit") //this is hacky, but .asl scripts need a "state" otherwise they won't update.
+{
+
+}
+startup 
+{
+//print("STARTUP");
+  // Add setting 'Split every N Saves', enabled by default
+  //settings.Add("split_n", true, "Split every N Saves");
+  
+  // Add setting 'Split on Save String', enabled by default
+  //settings.Add("split_string", true, "Split on Save String");
+  // Add setting 'Split on abandoned cores collected', enabled by default
+  //settings.Add("split_cores", true, "Split on abandoned cores collected");
+  
+  // Add setting 'Split on checkpoint coordinates', enabled by default
+  //settings.Add("split_CP", true, "Split on checkpoint coordinates");
+  //vars.splitVar = 0;
+  //vars.feature_vector_loading_english = new int[] { 3542, 143, 103, 92, 92, 101, 296, 2030, 3537, 142, 102, 92, 90, 100, 304, 2031, 3534, 141, 101, 92, 89, 101, 304, 2039, 3530, 140, 100, 92, 87, 101, 303, 2048, 3533, 138, 106, 92, 88, 105, 275, 2064, 3524, 140, 108, 94, 88, 104, 271, 2071, 3515, 142, 111, 96, 89, 104, 270, 2072, 3505, 145, 113, 98, 90, 105, 270, 2074, 2897, 128, 85, 78, 77, 81, 335, 2719, 2891, 126, 82, 77, 76, 81, 345, 2723, 2885, 124, 79, 74, 75, 82, 345, 2737, 2879, 121, 77, 72, 73, 82, 345, 2751, 3020, 122, 113, 58, 74, 95, 315, 2602, 3030, 122, 114, 58, 74, 95, 308, 2598, 3040, 122, 115, 57, 76, 94, 309, 2586, 3050, 122, 116, 57, 78, 92, 310, 2575, 3505, 150, 126, 75, 108, 83, 266, 2086, 3507, 151, 126, 75, 108, 83, 268, 2081, 3509, 151, 125, 77, 107, 85, 267, 2079, 3511, 152, 124, 78, 106, 86, 266, 2076, 3403, 139, 92, 96, 79, 111, 319, 2161, 3406, 137, 91, 95, 79, 110, 320, 2161, 3409, 137, 91, 94, 79, 109, 321, 2160, 3412, 136, 91, 94, 78, 109, 321, 2159 };
+  //vars.percent_of_bins_correct = 0.4f; //percentage of histogram bins which have to match for loading detection
+	//vars.variance_of_bins_allowed = 0.3f; //amount of variance allowed for correct comparison
+  //vars.currently_loading = false;
+  print("start");
+}
+reset
+{
+  print("reset");
+} 
+
+//isLoading
+//{
+//  return vars.currently_loading;
+//}
+
+
+update
+{
+  //How it works: 
+  //we capture the screen region around "LOADING"
+  //cut the screenshot region into patches
+  //compute a histogram of color values with a fixed amount of bins inside each patch
+  //serialize this into a vector
+  //and use this as a feature vector for comparison.
+  
+  //to check if we are in "LOADING" or not, we simply compare every new feature vector to a precomputed average of the load screen, with some tolerance
+  //to make it more robust (e.g. against Aku Aku flying inside the "LOADING" text)
+  //TODO: this probably only works @ 1080p atm, would need to increase screen region and patch size accordingly for e.g. 4k
+  //TODO: this currently requires either using Remote Play maximized on the primary screen or using your capture card preview maximized on the primary screen.
+  //TODO: also other languages might not work, I dunno
+  print("update"); 
+  System.Drawing.Bitmap b = new System.Drawing.Bitmap(100, 100);
+  print("update2");
+	using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(b))
+	{
+		g.CopyFromScreen(100 / 2, 100 / 2, 0, 0, new System.Drawing.Size(100, 100), System.Drawing.CopyPixelOperation.SourceCopy);
+    print("update3");
+	}
+  
+	
+
+}
---
+++ CrashNSaneLoadDetector.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace autosplittercnn_test
+{
+	partial class CrashNSaneLoadDetector
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.startButton = new System.Windows.Forms.Button();
+			this.imageDisplay = new System.Windows.Forms.PictureBox();
+			this.matchDisplayLabel = new System.Windows.Forms.Label();
+			this.pausedDisplay = new System.Windows.Forms.Panel();
+			this.gameTimeLabel = new System.Windows.Forms.Label();
+			this.realTimeLabel = new System.Windows.Forms.Label();
+			this.pausedTimeLabel = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.pauseSegmentList = new System.Windows.Forms.ListBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.recordCheck = new System.Windows.Forms.CheckBox();
+			this.saveDiagnosticCheck = new System.Windows.Forms.CheckBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.requiredMatchDisplayLabel = new System.Windows.Forms.Label();
+			this.recordCurrentButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.imageDisplay)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// startButton
+			// 
+			this.startButton.Location = new System.Drawing.Point(12, 397);
+			this.startButton.Name = "startButton";
+			this.startButton.Size = new System.Drawing.Size(75, 23);
+			this.startButton.TabIndex = 0;
+			this.startButton.Text = "Start";
+			this.startButton.UseVisualStyleBackColor = true;
+			this.startButton.Click += new System.EventHandler(this.startButton_Click);
+			// 
+			// imageDisplay
+			// 
+			this.imageDisplay.Location = new System.Drawing.Point(12, 5);
+			this.imageDisplay.Name = "imageDisplay";
+			this.imageDisplay.Size = new System.Drawing.Size(500, 200);
+			this.imageDisplay.TabIndex = 1;
+			this.imageDisplay.TabStop = false;
+			// 
+			// matchDisplayLabel
+			// 
+			this.matchDisplayLabel.AutoSize = true;
+			this.matchDisplayLabel.Location = new System.Drawing.Point(12, 234);
+			this.matchDisplayLabel.Name = "matchDisplayLabel";
+			this.matchDisplayLabel.Size = new System.Drawing.Size(13, 13);
+			this.matchDisplayLabel.TabIndex = 2;
+			this.matchDisplayLabel.Text = "0";
+			// 
+			// pausedDisplay
+			// 
+			this.pausedDisplay.Location = new System.Drawing.Point(130, 357);
+			this.pausedDisplay.Name = "pausedDisplay";
+			this.pausedDisplay.Size = new System.Drawing.Size(37, 35);
+			this.pausedDisplay.TabIndex = 3;
+			// 
+			// gameTimeLabel
+			// 
+			this.gameTimeLabel.AutoSize = true;
+			this.gameTimeLabel.Location = new System.Drawing.Point(12, 333);
+			this.gameTimeLabel.Name = "gameTimeLabel";
+			this.gameTimeLabel.Size = new System.Drawing.Size(43, 13);
+			this.gameTimeLabel.TabIndex = 4;
+			this.gameTimeLabel.Text = "0:00:00";
+			// 
+			// realTimeLabel
+			// 
+			this.realTimeLabel.AutoSize = true;
+			this.realTimeLabel.Location = new System.Drawing.Point(12, 290);
+			this.realTimeLabel.Name = "realTimeLabel";
+			this.realTimeLabel.Size = new System.Drawing.Size(43, 13);
+			this.realTimeLabel.TabIndex = 5;
+			this.realTimeLabel.Text = "0:00:00";
+			// 
+			// pausedTimeLabel
+			// 
+			this.pausedTimeLabel.AutoSize = true;
+			this.pausedTimeLabel.Location = new System.Drawing.Point(12, 379);
+			this.pausedTimeLabel.Name = "pausedTimeLabel";
+			this.pausedTimeLabel.Size = new System.Drawing.Size(43, 13);
+			this.pausedTimeLabel.TabIndex = 6;
+			this.pausedTimeLabel.Text = "0:00:00";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(8, 208);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(142, 22);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "Current Matches";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(8, 268);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(92, 22);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Real Time";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(8, 311);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(103, 22);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Game Time";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(8, 357);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(116, 22);
+			this.label3.TabIndex = 10;
+			this.label3.Text = "Paused Time";
+			// 
+			// pauseSegmentList
+			// 
+			this.pauseSegmentList.FormattingEnabled = true;
+			this.pauseSegmentList.Location = new System.Drawing.Point(221, 299);
+			this.pauseSegmentList.Name = "pauseSegmentList";
+			this.pauseSegmentList.Size = new System.Drawing.Size(142, 121);
+			this.pauseSegmentList.TabIndex = 11;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(217, 269);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(146, 22);
+			this.label4.TabIndex = 12;
+			this.label4.Text = "Pause Segments";
+			// 
+			// recordCheck
+			// 
+			this.recordCheck.AutoSize = true;
+			this.recordCheck.Location = new System.Drawing.Point(369, 299);
+			this.recordCheck.Name = "recordCheck";
+			this.recordCheck.Size = new System.Drawing.Size(105, 17);
+			this.recordCheck.TabIndex = 13;
+			this.recordCheck.Text = "Record Features";
+			this.recordCheck.UseVisualStyleBackColor = true;
+			this.recordCheck.CheckedChanged += new System.EventHandler(this.recordCheck_CheckedChanged);
+			// 
+			// saveDiagnosticCheck
+			// 
+			this.saveDiagnosticCheck.AutoSize = true;
+			this.saveDiagnosticCheck.Checked = true;
+			this.saveDiagnosticCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.saveDiagnosticCheck.Location = new System.Drawing.Point(369, 322);
+			this.saveDiagnosticCheck.Name = "saveDiagnosticCheck";
+			this.saveDiagnosticCheck.Size = new System.Drawing.Size(146, 17);
+			this.saveDiagnosticCheck.TabIndex = 14;
+			this.saveDiagnosticCheck.Text = "Save Disagnostic Images";
+			this.saveDiagnosticCheck.UseVisualStyleBackColor = true;
+			this.saveDiagnosticCheck.CheckedChanged += new System.EventHandler(this.saveDiagnosticCheck_CheckedChanged);
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(183, 208);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(249, 22);
+			this.label6.TabIndex = 15;
+			this.label6.Text = "Required Matches (for Pause)";
+			// 
+			// requiredMatchDisplayLabel
+			// 
+			this.requiredMatchDisplayLabel.AutoSize = true;
+			this.requiredMatchDisplayLabel.Location = new System.Drawing.Point(184, 234);
+			this.requiredMatchDisplayLabel.Name = "requiredMatchDisplayLabel";
+			this.requiredMatchDisplayLabel.Size = new System.Drawing.Size(25, 13);
+			this.requiredMatchDisplayLabel.TabIndex = 16;
+			this.requiredMatchDisplayLabel.Text = "290";
+			// 
+			// recordCurrentButton
+			// 
+			this.recordCurrentButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.recordCurrentButton.Location = new System.Drawing.Point(93, 398);
+			this.recordCurrentButton.Name = "recordCurrentButton";
+			this.recordCurrentButton.Size = new System.Drawing.Size(116, 23);
+			this.recordCurrentButton.TabIndex = 17;
+			this.recordCurrentButton.Text = "Record Current Feature";
+			this.recordCurrentButton.UseVisualStyleBackColor = true;
+			this.recordCurrentButton.Click += new System.EventHandler(this.recordCurrentButton_Click);
+			// 
+			// CrashNSaneLoadDetector
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(526, 432);
+			this.Controls.Add(this.recordCurrentButton);
+			this.Controls.Add(this.requiredMatchDisplayLabel);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.saveDiagnosticCheck);
+			this.Controls.Add(this.recordCheck);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.pauseSegmentList);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.pausedTimeLabel);
+			this.Controls.Add(this.realTimeLabel);
+			this.Controls.Add(this.gameTimeLabel);
+			this.Controls.Add(this.pausedDisplay);
+			this.Controls.Add(this.matchDisplayLabel);
+			this.Controls.Add(this.imageDisplay);
+			this.Controls.Add(this.startButton);
+			this.Name = "CrashNSaneLoadDetector";
+			this.Text = "Crash N Sane Trilogy Load Detector";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CrashNSaneLoadDetector_FormClosing);
+			((System.ComponentModel.ISupportInitialize)(this.imageDisplay)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button startButton;
+		private System.Windows.Forms.PictureBox imageDisplay;
+		private System.Windows.Forms.Label matchDisplayLabel;
+		private System.Windows.Forms.Panel pausedDisplay;
+		private System.Windows.Forms.Label gameTimeLabel;
+		private System.Windows.Forms.Label realTimeLabel;
+		private System.Windows.Forms.Label pausedTimeLabel;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ListBox pauseSegmentList;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.CheckBox recordCheck;
+		private System.Windows.Forms.CheckBox saveDiagnosticCheck;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label requiredMatchDisplayLabel;
+		private System.Windows.Forms.Button recordCurrentButton;
+	}
+}
+
